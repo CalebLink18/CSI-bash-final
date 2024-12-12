47d469d52b733e7889de8df6410d3c8b9ba28aa5
Caleb <caleb.link@mymail.champlain.edu>
2024-12-12 18:24:47 -0500

    second bash script

---
+++ logIOCchecker.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function filter_apache_logs() {
+
+local log_file="$1"
+local ioc_file="$2"
+
+#check if inputs exist
+if [[ ! -f "$log_file" ]]; then
+	echo "Apache log file error."
+	return 1
+fi
+if [[ ! -f "$iov_file" ]]; then
+	echo "IOC.txt error"
+	return 1
+fi
+
+#filters log entries what is inside of IOC.txt
+while IFS= read -r ioc; do
+	if [[ -n "$ioc" ]]; then
+		mapfile -t output < <(grep -F "$ioc" "$log_file")
+	fi
+done < "$ioc_file"
+
+for line in "${output[@]}"; do
+	echo "$line"
+done
+
+}
